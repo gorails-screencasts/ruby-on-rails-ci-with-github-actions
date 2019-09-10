2cd11f5efd55cae7ce3858ed71146c0ee1fe17e0
Chris Oliver <excid3@gmail.com>
2019-09-10 16:17:05 -0500

    Add helper methods using TDD

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,16 @@ class Rating < ApplicationRecord
   belongs_to :person
 
   validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+
+  def promoter?
+    score >= 9 && score <= 10
+  end
+
+  def passive?
+    score >= 7 && score <= 8
+  end
+
+  def detractor?
+    score <= 6 && score >= 0
+  end
 end
--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -20,4 +20,61 @@ class RatingTest < ActiveSupport::TestCase
     rating.valid?
     assert_not rating.errors[:score].empty?
   end
+
+  test "promoter? returns true if score is 9 or 10" do
+    9.upto(10).each do |i|
+      rating = Rating.new(score: i)
+      assert rating.promoter?
+    end
+  end
+
+  test "promoter? returns false if score is less than 9" do
+    rating = Rating.new(score: 1)
+    assert_not rating.promoter?
+  end
+
+  test "promoter? returns false if score is greater than 10" do
+    rating = Rating.new(score: 11)
+    assert_not rating.promoter?
+  end
+
+  test "passive? returns true if score is 7 or 8" do
+    7.upto(8).each do |i|
+      rating = Rating.new(score: i)
+      assert rating.passive?
+    end
+  end
+
+  test "passive? returns false if score is less than 7" do
+    0.upto(6).each do |i|
+      rating = Rating.new(score: i)
+      assert_not rating.passive?
+    end
+  end
+
+  test "passive? returns false if score is greater than 8" do
+    9.upto(10).each do |i|
+      rating = Rating.new(score: i)
+      assert_not rating.passive?
+    end
+  end
+
+  test "detractor? returns true if score is less than or equal to 6" do
+    0.upto(6).each do |i|
+      rating = Rating.new(score: i)
+      assert rating.detractor?
+    end
+  end
+
+  test "detractor? returns false if score is greater than 6" do
+    7.upto(10).each do |i|
+      rating = Rating.new(score: i)
+      assert_not rating.detractor?
+    end
+  end
+
+  test "detractor? returns false if score is less than 0" do
+    rating = Rating.new(score: -1)
+    assert_not rating.detractor?
+  end
 end
